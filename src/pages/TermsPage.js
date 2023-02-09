9e9dc11102615ccfdcc77fcb8c6f703525081518
Thanasis Balamatsis <thanasis.b@yahoo.com>
2023-02-09 12:05:53 +0200

    Rename file

--- src/pages/TermsPage.js
+++ src/pages/TermsPage.js
@@ -1,11 +1,11 @@
 import useTermsContext from '../hooks/useTermsContext';
 
-export default function FoundTermsPage() {
+export default function TermsPage() {
     const {foundTerms} = useTermsContext();
 
     const renderedTerms = foundTerms.map((term) => {
         return (
-            <div key={term.id}>{term}</div>
+            <div key={term.pageid}>{term}</div>
         );
     });
 
