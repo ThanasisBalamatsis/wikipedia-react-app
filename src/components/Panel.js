38e63ba5cb9b4eb2b040b0829d569f4857028814
Thanasis Balamatsis <thanasis.b@yahoo.com>
2023-02-21 18:27:23 +0200

    Update style in Panel.js

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -6,7 +6,7 @@ export default function Panel({children, searchPanel, termPanel, term, ...rest})
         "place-items-center border rounded p-3 shadow ", 
         {
             "w-full grid bg-stone-800": searchPanel,
-            "w-5/6 m-6 h-64 place-self-center text-center bg-stone-100": termPanel
+            "w-5/6 m-6 h-48 place-self-center text-center bg-stone-100": termPanel
         }
     );
 
@@ -14,7 +14,7 @@ export default function Panel({children, searchPanel, termPanel, term, ...rest})
     const content = (termPanel) ? <div>
                                     <div className="font-thin text-xl mb-2">{children}</div>
                                     <div  className="flex justify-center">
-                                        <div className="w-5/6 h-40 bg-stone-50 rounded font-thin text-sm">{term.snippet.replace(/<[^>]+>/g, '')}</div>
+                                        <div className="w-5/6 h-32 rounded font-thin text-sm">{term.snippet.replace(/<[^>]+>/g, '')}</div>
                                     </div>
                                   </div> 
                                   : children;
