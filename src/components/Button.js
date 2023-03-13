380e84170e95023bf8e7b0da270abe50c92276cc
Thanasis Balamatsis <thanasis.b@yahoo.com>
2023-03-13 19:19:43 +0200

    Implement Modal

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,13 +4,15 @@ export default function Button({
     children,
     submit,
     close,
+    save,
     read,
     ...rest
 }) {
     const classes = classnames("flex items-center px-3 py-1.5 border h-9 m-1 font-thin",{
         "border-sky-600 bg-sky-600 text-white rounded" : submit,
         "border-red-400 bg-red-500" : close,
-        "border-bg-stone-300 bg-stone-300 text-stone-600 text-sm rounded-lg h-8 w-1/6 justify-center" : read
+        "border-bg-stone-300 bg-stone-300 text-stone-600 text-sm rounded-lg h-8 w-1/6 justify-center" : read,
+        "border-bg-red-300 bg-red-300 text-red-600 text-sm rounded-lg h-8 w-1/6 justify-center" : save,
     });
 
     return (
@@ -21,8 +23,8 @@ export default function Button({
 }
 
 Button.propTypes = {
-    checkVariationValue: ({submit, close, read}) => {
-        const count = Number(!!submit) + Number(!!close) + Number(!!read);
+    checkVariationValue: ({submit, close, read, save}) => {
+        const count = Number(!!submit) + Number(!!close) + Number(!!read) + Number(!!save);
 
         if (count > 1) 
             return new Error("Button can have only either submit or close property");
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import classnames from "classnames";
 import Button from "./Button";
-import {FiBookOpen} from "react-icons/fi"; 
+import Modal from "./Modal";
+import {FiBookOpen} from "react-icons/fi";
+import { GoX } from "react-icons/go";
 
 export default function Panel({children, searchPanel, termPanel, term, ...rest}) {
 
@@ -8,17 +11,40 @@ export default function Panel({children, searchPanel, termPanel, term, ...rest})
         "place-items-center border rounded p-3 shadow ", 
         {
             "w-full grid bg-stone-800": searchPanel,
-            "w-5/6 m-6 h-48 place-self-center text-center bg-stone-100": termPanel
+            "w-5/6 m-6 h-60 place-self-center text-center bg-stone-100": termPanel
         }
     );
 
+    const [showModal, setShowModal] = useState(false);
+
+    const onClose = () => {
+        setShowModal(false);
+    }
+
+    const onSave = () => {
+        console.log('clicked save!');
+    }
+
+    const showText = () => {
+        setShowModal(true);
+    }
+
+    const actionBar = <div>
+        <Button onClick={onClose} close><GoX/></Button>
+        <Button onClick={onSave} close></Button>
+    </div>;
+
+    const modal = <Modal onClose={onClose} actionBar={actionBar}>
+        <p>Here is the modal</p>
+    </Modal>;
 
     const content = (termPanel) ? <div>
                                     <div className="font-thin text-xl mb-2">{children}</div>
                                     <div  className="flex justify-center">
                                         <div className="w-5/6 h-24 rounded font-thin text-sm">{term.snippet.replace(/<[^>]+>/g, '')}</div>
                                     </div>
-                                    <Button read><FiBookOpen/></Button>
+                                    <Button read onClick={showText}><FiBookOpen/></Button>
+                                    {showModal && modal}
                                   </div> 
                                   : children;
 
